413a5fc30a524d59effe3d7cc776b18eba343cc9
Ioan <ioan.zicu94@gmail.com>
2021-10-23 14:59:30 +0200

    added search for ads

--- ads/views.py
+++ ads/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 from django.http import HttpResponse
 from django.db.utils import IntegrityError
 from django.urls.base import reverse_lazy, reverse
@@ -6,6 +7,7 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.base import View
+from django.contrib.humanize.templatetags.humanize import naturaltime
 
 from ads.models import Ad, Comment, Fav
 from ads.forms import CreateForm, CommentForm
@@ -17,15 +19,50 @@ class AdListView(OwnerListView):
     template_name = 'ads/ad_list.html'
 
     def get(self, request):
-        ad_list = Ad.objects.all()
         favorites = list()
         if request.user.is_authenticated:
             # rows = [{'id': 2}, {'id': 4} ... ]  (A list of rows)
             rows = request.user.favorite_ads.values('id')
             # favorites = [2, 4, ...] using list comprehension
             favorites = [row['id'] for row in rows]
+
+        strval = request.GET.get('search', False)
+        if strval:
+            # Simple title-only search
+            # objects = Post.objects.filter(title__contains=strval).select_related().order_by('-updated_at')[:10]
+
+            # Multi-field search
+            # __icontains for case-insensitive search
+
+            query = Q(title__icontains=strval)
+            query.add(Q(text__icontains=strval), Q.OR)
+            ad_list = Ad.objects.filter(
+                query).select_related().order_by('-updated_at')[:10]
+        else:
+            ad_list = Ad.objects.all()[:10]
+
+        # Augment the post_list
+        for obj in ad_list:
+            obj.natural_updated = naturaltime(obj.updated_at)
+
         context = {'ad_list': ad_list, 'favorites': favorites}
         return render(request, self.template_name, context)
+
+
+# References
+
+# https://docs.djangoproject.com/en/3.0/topics/db/queries/#one-to-many-relationships
+
+# Note that the select_related() QuerySet method recursively prepopulates the
+# cache of all one-to-many relationships ahead of time.
+
+# sql “LIKE” equivalent in django query
+# https://stackoverflow.com/questions/18140838/sql-like-equivalent-in-django-query
+
+# How do I do an OR filter in a Django query?
+# https://stackoverflow.com/questions/739776/how-do-i-do-an-or-filter-in-a-django-query
+
+# https://stackoverflow.com/questions/1074212/how-can-i-see-the-raw-sql-queries-django-is-running
 
 
 class AdDetailView(OwnerDetailView):
--- well/views.py
+++ well/views.py
@@ -7,28 +7,30 @@ from well.utils import dump_queries
 
 from django.db.models import Q
 
+
 class PostListView(View):
     template_name = "well/list.html"
 
-    def get(self, request) :
-        strval =  request.GET.get("search", False)
-        if strval :
+    def get(self, request):
+        strval = request.GET.get("search", False)
+        if strval:
             # Simple title-only search
             # objects = Post.objects.filter(title__contains=strval).select_related().order_by('-updated_at')[:10]
 
             # Multi-field search
             # __icontains for case-insensitive search
-            query = Q(title__icontains=strval) 
+            query = Q(title__icontains=strval)
             query.add(Q(text__icontains=strval), Q.OR)
-            post_list = Post.objects.filter(query).select_related().order_by('-updated_at')[:10]
-        else :
+            post_list = Post.objects.filter(
+                query).select_related().order_by('-updated_at')[:10]
+        else:
             post_list = Post.objects.all().order_by('-updated_at')[:10]
 
         # Augment the post_list
         for obj in post_list:
             obj.natural_updated = naturaltime(obj.updated_at)
 
-        ctx = {'post_list' : post_list, 'search': strval}
+        ctx = {'post_list': post_list, 'search': strval}
         return render(request, self.template_name, ctx)
 
 # References
